17d9ec7c2c681432640001f54a90810d7ca6f684
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-04-19 18:33:56 +0300

    Оптуна

--- optune/optune_ocsort.py
+++ optune/optune_ocsort.py
@@ -1,8 +1,6 @@
-import optuna
-from optuna.study import StudyDirection
 
 from configs import get_detections_path
-from optune.optune_tools import save_result, reset_seed
+from optune.optune_tools import reset_seed, common_run_optuna
 from yolo_optune import run_track_yolo
 
 
@@ -41,21 +39,22 @@ def objective_ocsort(trial):
     change_bb = None  # pavel_change_bbox  # change_bbox
 
     test_func = "timur"
-    test_func = "popov_alex"
+    # test_func = "popov_alex"
+    # test_func = "group3"
 
     txt_source_folder = str(get_detections_path())
 
     det_thresh = trial.suggest_float('det_thresh', 0.3, 0.6, step=0.1)
 
-    # max_age = trial.suggest_int('max_age', 1, 10, log=True)
-    max_age = int(trial.suggest_categorical('max_age', [3, 10, 50, 100]))
+    max_age = trial.suggest_int('max_age', 1, 20, log=True)
+    # max_age = int(trial.suggest_categorical('max_age', [3, 10, 50, 100]))
 
     min_hits = trial.suggest_int('min_hits', 6, 8)
     iou_threshold = trial.suggest_float('iou_threshold', 0.6, 0.8, step=0.1)
     delta_t = trial.suggest_int('delta_t', 5, 15)
-    asso_func = trial.suggest_categorical('asso_func', ["iou", "giou"])
+    asso_func = trial.suggest_categorical('asso_func', ["iou"])
     inertia = trial.suggest_float('inertia', 0.6, 0.8, step=0.1)
-    use_byte = trial.suggest_categorical('use_byte', [True, False])
+    use_byte = trial.suggest_categorical('use_byte', [True])
 
     # test_func = trial.suggest_categorical('test_func', ["timur", "group_3"])
 
@@ -85,57 +84,12 @@ def objective_ocsort(trial):
 
 
 def run_optuna():
-    study = optuna.create_study(direction=StudyDirection.MAXIMIZE)
-    study.optimize(objective_ocsort, n_trials=3, show_progress_bar=True)
+    output_folder = "d:\\AI\\2023\\corridors\\dataset-v1.1\\Optune"
 
-    trial = study.best_trial
-
-    print(f"Accuracy: {trial.value}")
-    print(f"Best hyper parameters: {trial.params}")
-
-    output_folder = "d:\\AI\\2023\\corridors\\dataset-v1.1\\"
-
-    save_result(trial, output_folder, "ocsort")
-
-
-def test():
-    video_source = "d:\\AI\\2023\\corridors\\dataset-v1.1\\test\\"
-
-    files = None
-    # files = ['1', "2", "3"]
-    files = ["3"]
-
-    # classes = [0]
-    classes = None
-
-    change_bb = None  # pavel_change_bbox  # change_bbox
-
-    test_func = "timur"
-
-    txt_source_folder = str(get_detections_path())
-    tracker_name = "ocsort"
-    tracker_config = \
-        {
-            tracker_name:
-                {
-                    "asso_func": "giou",
-                    "det_thresh": 0.4094083246275299,
-                    "delta_t": 6,
-                    "inertia": 0.6066896482364373,
-                    "iou_thresh": 0.629302262088819,
-                    "max_age": 5,
-                    "min_hits": 7,
-                    "use_byte": True
-                }
-        }
-
-    cmp_results = run_track_yolo(txt_source_folder, video_source, tracker_name, tracker_config,
-                                 test_func=test_func,
-                                 files=files, change_bb=change_bb, classes=classes)
-
-    accuracy = cmp_results["total_equal_percent"]
-
-    print(f"accuracy = {accuracy}")
+    common_run_optuna(tracker_tag="ocsort",
+                      output_folder=output_folder,
+                      objective=objective_ocsort,
+                      trials=100)
 
 
 if __name__ == '__main__':
