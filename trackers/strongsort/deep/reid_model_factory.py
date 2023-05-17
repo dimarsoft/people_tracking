8f841efc0e6bf24b5ff76a3d12d4cbee257849bb
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-05-17 23:03:06 +0300

    fix return value

--- trackers/strongsort/deep/reid_model_factory.py
+++ trackers/strongsort/deep/reid_model_factory.py
@@ -109,7 +109,7 @@ def get_model_url(model):
     if model.name in __trained_urls:
         return __trained_urls[model.name]
     else:
-        None
+        return None
 
 
 def is_model_in_model_types(model):
