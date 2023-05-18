a87b34429823b1c46ebc63d976a2f10ad50753d0
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-05-18 16:25:57 +0300

    refactoring after pylint

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-__version__ = 1.2
+"""
+Основной модуля для запуска задач
+"""
 
 import argparse
 from pathlib import Path
 from typing import Union, Optional
 
-import torch
-
 from post_processing.group_1_detect import group_1_detect
 from post_processing.vladimir_detect import vladimir_detect
-from utils.torch_utils import date_modified, git_describe, git_branch_name
+from tools.version_tool import get_version
 from yolo_common.yolo_track_main import get_results_video_yolo
 
+__version__ = 1.2
+"""
+Версия модуля
+"""
 
-def print_version():
-    git_info = git_describe()
 
-    if git_info is None:
-        git_info = f"{date_modified()}"
-    else:
-        git_info = f"git: {git_info}, {date_modified()}"
+def print_version() -> None:
+    """
+    Вывод версии модуля
+    Returns
+    -------
 
-    version = f'Version: {__version__}, {git_info}, torch {torch.__version__}'  # string
+    """
+
+    soft_version = get_version(__version__, __file__)
+
+    version = f'Version: {soft_version}'
     print(f"Humans, helmets and uniforms. {version}")
 
 
-def get_version(version=__version__, path=__file__) -> str:
-    git_info = git_describe()
-    branch_name = git_branch_name()
-
-    if git_info is None:
-        git_info = f"{date_modified(path)}"
-    else:
-        git_info = f"git: {branch_name}:{git_info}, {date_modified()}"
-
-    return f'{version}, {git_info}, torch {torch.__version__}'
-
-
 def create_empty_dict(video_source) -> dict:
+    """
+    Создание пустого ответа.
+    Parameters
+    ----------
+    video_source
+    Видео файл
+
+    Returns
+    -------
+    Словарь
+
+    """
     results = \
         {
             "file": str(video_source),
@@ -53,7 +60,8 @@ def run_group1_detection(video_source: Union[str, Path],
     """
     Вызов обработки видео файла кодом группы №1.
     :param video_source: Путь к файлу.
-    :param model: Путь к модели, можно не указывать, тогда загрузится с гуглдиска.
+    :param model: Путь к модели, можно не указывать,
+                  тогда загрузится с гуглдиска.
     :return: Возвращаем словарь с результатом
     """
     return group_1_detect(video_source, model_path=model)
@@ -64,6 +72,27 @@ def run_group2_detection(video_source: Union[str, Path],
                          tracker_name: Optional[str] = None,
                          tracker_config: Union[str, dict, Path, None] = None,
                          log: bool = False) -> dict:
+    """
+    Детекция людей и нарушений группа №2
+    Parameters
+    ----------
+    video_source
+        Видео файл
+    model
+        Модель
+
+    tracker_name
+        Имя трекера
+    tracker_config
+        Настройки трекера
+    log
+        Вкл/выкл логирования
+
+    Returns
+    -------
+        Словарь с результатом
+
+    """
     if tracker_name is None:
         tracker_name = "fastdeepsort"
     return get_results_video_yolo(video_source,
@@ -75,6 +104,20 @@ def run_group2_detection(video_source: Union[str, Path],
 
 def run_vladimir_detection(video_source: Union[str, Path],
                            model: Union[str, Path, None] = None) -> dict:
+    """
+    Детекция Владимира
+    Parameters
+    ----------
+    video_source
+        Видео файл
+    model
+        Модель
+
+    Returns
+    -------
+        Словарь с результатом
+
+    """
     return vladimir_detect(video_source,
                            model_path=model)
 
@@ -84,6 +127,26 @@ def run_detection(version: Union[str, int],
                   model: Union[str, Path, None] = None,
                   tracker_name: Optional[str] = None,
                   tracker_config: Union[str, dict, Path, None] = None) -> Optional[dict]:
+    """
+    Запуск детекции
+    Parameters
+    ----------
+    version
+        Версия детекции 1, 2, 3
+    source
+        Видео файл
+    model
+        Модель
+    tracker_name
+        Имя трекера
+    tracker_config
+        Настройки трекера
+
+    Returns
+    -------
+        Словарь с результатом
+
+    """
     if version in (1, "1"):
         return run_group1_detection(source, model=model)
 
@@ -110,7 +173,8 @@ def run_cli(opt_info) -> dict:
     Словарь с результатом
     """
     version, source, tracker_name, tracker_config, model = \
-        opt_info.version, opt_info.source, opt_info.tracker_name, opt_info.tracker_config, opt_info.model
+        opt_info.version, opt_info.source, opt_info.tracker_name, \
+        opt_info.tracker_config, opt_info.model
 
     return run_detection(version=version, source=source,
                          tracker_name=tracker_name, tracker_config=tracker_config,
@@ -123,11 +187,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--version', type=str,
-                        help='version of post processing: "1" - group 1, "2" - group 2, 3 - Владимир')  #
+                        help='version of post processing: "1" - group 1, "2" - group 2, '
+                             '3 - Владимир')  #
     parser.add_argument('--source', type=str, help='path to video file')  #
     parser.add_argument('--model', type=str, default=None, help='path to yolo model')  #
     parser.add_argument('--tracker_name', type=str, default=None, help='tracker_name')
-    parser.add_argument('--tracker_config', type=str, default=None, help='tracker_config: dict, file')
+    parser.add_argument('--tracker_config', type=str, default=None,
+                        help='tracker_config: dict, file')
 
     opt = parser.parse_args()
     print(opt)
