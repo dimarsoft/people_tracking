a9a65ee11b83a5acd4e4e2748748f72220bf2fd1
Дмитрий Марандич <dimar_soft@hotmail.com>
2023-05-20 20:25:20 +0300

    typing, docstrings

--- yolo_common/yolo_track_main.py
+++ yolo_common/yolo_track_main.py
@@ -1,5 +1,6 @@
 """
-
+Модуль для запуска детекции и трекинга.
+Функции для скачивания моделей.
 """
 import json
 from pathlib import Path
@@ -24,16 +25,32 @@ yolo7_model_gdrive = "https://drive.google.com/drive/u/4/folders/1b-tp_yxHgadeEl
 yolo7_model_gdrive_file = "25.02.2023_dataset_1.1_yolov7_best.pt"
 yolo8_model_gdrive_file = "640img_8x_best_b16_e10.pt"
 
-test_video_share_folder_link = "https://drive.google.com/drive/folders/1YK0a3peuwdbvoZUAKciCvYM5KjKeizA6?usp=sharing"
+test_video_share_folder_link = \
+    "https://drive.google.com/drive/folders/1YK0a3peuwdbvoZUAKciCvYM5KjKeizA6?usp=sharing"
 
-test_video_share_folder_link_1_85 = "https://drive.google.com/drive/folders/1o8xlmtVJkmvH7nDfFOQoBCoyRnhy9aVQ"
+test_video_share_folder_link_1_85 = \
+    "https://drive.google.com/drive/folders/1o8xlmtVJkmvH7nDfFOQoBCoyRnhy9aVQ"
 
 # для турникета
-yolo8_turniket_model_gdrive_file = "2023_05_12_17_52_39_yolo8_train_yolov8n.pt_epochs_30_batch_8_single_cls_best.pt"
-yolo8_turniket_model_gdrive_link = "https://drive.google.com/uc?id=1-dbgib8A6Tg4KLPbE2uZx7VdmAEUE7a0"
+yolo8_turniket_model_gdrive_file = \
+    "2023_05_12_17_52_39_yolo8_train_yolov8n.pt_epochs_30_batch_8_single_cls_best.pt"
+yolo8_turniket_model_gdrive_link = \
+    "https://drive.google.com/uc?id=1-dbgib8A6Tg4KLPbE2uZx7VdmAEUE7a0"
 
 
 def get_local_path(yolo_version: YoloVersion) -> Path:
+    """
+    Получить путь к файлу модели на локальном диске
+    Parameters
+    ----------
+    yolo_version
+        Версия YOLO
+
+    Returns
+    -------
+        Путь
+
+    """
     if yolo_version == YoloVersion.yolo_v7:
         return Path(WEIGHTS) / yolo7_model_gdrive_file
 
@@ -41,6 +58,18 @@ def get_local_path(yolo_version: YoloVersion) -> Path:
 
 
 def get_turniket_local_path(yolo_version: YoloVersion) -> Path:
+    """
+    Получить путь к файлу модели для детекции турникета на локальном диске
+    Parameters
+    ----------
+    yolo_version
+    Версия YOLO, поддерживается только 8
+
+    Returns
+    -------
+    Путь
+
+    """
     if yolo_version == YoloVersion.yolo_v7:
         raise Exception(f"{YoloVersion.yolo_v7} is not supported")
 
@@ -48,6 +77,18 @@ def get_turniket_local_path(yolo_version: YoloVersion) -> Path:
 
 
 def get_link(yolo_version: YoloVersion) -> str:
+    """
+    Получить ссылку на гугл диск для скачивания модели
+    Parameters
+    ----------
+    yolo_version
+        Версия YOLO
+
+    Returns
+    -------
+    Строку с ссылкой
+
+    """
     if yolo_version == YoloVersion.yolo_v7:
         return 'https://drive.google.com/uc?id=1U6zt4rOy2v3VrLjMrsqdHF_Y6k9INbib'
 
@@ -107,12 +148,13 @@ def download_test_video():
 
     print(f"download {output} from {url}")
 
-    folders = gdown.download_folder(id="1YK0a3peuwdbvoZUAKciCvYM5KjKeizA6", output=output, quiet=False)
+    folders = gdown.download_folder(id="1YK0a3peuwdbvoZUAKciCvYM5KjKeizA6",
+                                    output=output, quiet=False)
     # folders = gdown.download_folder(url=url, quiet=False)
     print(folders)
 
 
-def download_test_video_1_85(output):
+def download_test_video_1_85(output: Union[str, Path]):
     output = str(output)
 
     folders = gdown.download_folder(id="1o8xlmtVJkmvH7nDfFOQoBCoyRnhy9aVQ",
@@ -121,7 +163,7 @@ def download_test_video_1_85(output):
     print(folders)
 
 
-def post_process(test_func, track, num, w, h, bound_line, source) -> Result:
+def post_process(test_func, track: list, num, w, h, bound_line, source) -> Result:
     # count humans
 
     humans_result = Result(0, 0, 0, [])
@@ -130,7 +172,8 @@ def post_process(test_func, track, num, w, h, bound_line, source) -> Result:
         try:
             tracks_new = []
             for item in track:
-                tracks_new.append([item[0], item[5], item[6], item[1], item[2], item[3], item[4], item[7]])
+                tracks_new.append([item[0], item[5], item[6], item[1], item[2],
+                                   item[3], item[4], item[7]])
 
             if isinstance(test_func, str):
 
@@ -230,7 +273,8 @@ def run_single_video_yolo(source, yolo_info="7", conf=0.3, iou=0.45, test_func="
 
 
 def get_results_video_yolo_txt(source, conf=0.3, test_func="timur",
-                               tracker_type="fastdeepsort", log: bool = True, ext: str = "txt") -> dict:
+                               tracker_type="fastdeepsort",
+                               log: bool = True, ext: str = "txt") -> dict:
     txt_source_folder = str(get_detections_path())
 
     all_trackers = get_all_optune_trackers()
@@ -261,7 +305,8 @@ def get_results_video_yolo_txt(source, conf=0.3, test_func="timur",
     cameras_info = load_default_bound_line()
     bound_line = get_bound_line(cameras_info, num)
 
-    humans_result = get_post_process_results(test_func, track, num, w, h, fps, bound_line, source, log=True)
+    humans_result = get_post_process_results(test_func, track,
+                                             num, w, h, fps, bound_line, source, log=True)
 
     res = json.loads(results_to_json(humans_result))
     res["fps"] = fps
@@ -271,7 +316,8 @@ def get_results_video_yolo_txt(source, conf=0.3, test_func="timur",
 
 def get_results_video_yolo(source, yolo_info="7", conf=0.3, iou=0.45, test_func="timur",
                            model: Union[str, Path, None] = None,
-                           tracker_type="fastdeepsort", tracker_config=None, log: bool = True) -> dict:
+                           tracker_type="fastdeepsort",
+                           tracker_config=None, log: bool = True) -> dict:
     num, w, h, fps = get_camera(source)
 
     print(f"yolo version = {yolo_info}")
@@ -310,7 +356,8 @@ def get_results_video_yolo(source, yolo_info="7", conf=0.3, iou=0.45, test_func=
     cameras_info = load_default_bound_line()
     bound_line = get_bound_line(cameras_info, num)
 
-    humans_result = get_post_process_results(test_func, track, num, w, h, fps, bound_line, source, log=True)
+    humans_result = get_post_process_results(test_func, track, num, w, h, fps,
+                                             bound_line, source, log=True)
 
     results = results_to_dict(humans_result)
     # res = json.loads(results_to_json(humans_result))
