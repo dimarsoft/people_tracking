7b4ce448a6ad00bff2e8d2ed32ba412b32306639
Dmitrii Marandich <dimar_soft@yahoo.com>
2025-01-17 23:10:04 +0300

    show torch version.

--- torch_gpu.py
+++ torch_gpu.py
@@ -1,16 +1,18 @@
+"""
+Вывод версии torch.
+"""
 import torch
 
 
-def show_torch():
+def show_torch() -> None:
     """
     Версии PyTorch
     Returns:
 
     """
     print(f'PyTorch version: {torch.__version__}')
+    print(f'CUDA version: {torch.version.cuda}')
     print(f'CUDNN version: {torch.backends.cudnn.version()}')
     print(f'Available GPU devices: {torch.cuda.device_count()}')
     print(f'Device Name: {torch.cuda.get_device_name()}')
-
 show_torch()
-
